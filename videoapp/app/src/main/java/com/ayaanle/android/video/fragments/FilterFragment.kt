15a52640f1f908f58c40038f1783cb66804efe0c
Ayaanle hashi <91608871+ayaanlehashi11@users.noreply.github.com>
2023-03-16 09:54:30 +0300

    Update FilterFragment.kt

--- videoapp/app/src/main/java/com/ayaanle/android/video/fragments/FilterFragment.kt
+++ videoapp/app/src/main/java/com/ayaanle/android/video/fragments/FilterFragment.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.camera2.video.fragments
+package com.ayaanle.android.video.fragments
 
 import android.annotation.SuppressLint
 import android.content.Context
@@ -32,8 +32,8 @@ import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.camera.utils.GenericListAdapter
-import com.example.android.camera2.video.R
+import com.ayaanle.android.video.utils.GenericListAdapter
+import com.example.android.video.R
 
 /**
  * In this [Fragment] we let users pick whether or not the portrait filter is on.
